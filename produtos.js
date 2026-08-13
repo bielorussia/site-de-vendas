63d417c221068015e22ac00a40b3dd248e94752b
bielorussia <gabrielperezsilva2000@gmail.com>
2026-08-13 19:48:46 -0300

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,701 @@
+const produtos = [
+    // =========================
+    // DRAMA
+    // =========================
+
+    {
+        id: 1,
+        nome: "A Cinco Passos de Você",
+        autor: "Rachael Lippincott",
+        genero: "Drama",
+        preco: 39.90,
+        imagem: "drama/acincopassosdevoce.jpeg"
+    },
+
+    {
+        id: 2,
+        nome: "A Culpa é das Estrelas",
+        autor: "John Green",
+        genero: "Drama",
+        preco: 34.90,
+        imagem: "drama/aculpaedasestrelas.jpeg"
+    },
+
+    {
+        id: 3,
+        nome: "As Vantagens de Ser Invisível",
+        autor: "Stephen Chbosky",
+        genero: "Drama",
+        preco: 39.90,
+        imagem: "drama/asvantagensdeserinvisivel.jpeg"
+    },
+
+    {
+        id: 4,
+        nome: "Como Eu Era Antes de Você",
+        autor: "Jojo Moyes",
+        genero: "Drama",
+        preco: 39.90,
+        imagem: "drama/comoeuerantesdevoce.jpeg"
+    },
+
+    {
+        id: 5,
+        nome: "Extraordinário",
+        autor: "R. J. Palacio",
+        genero: "Drama",
+        preco: 34.90,
+        imagem: "drama/extraordinario.jpeg"
+    },
+
+    {
+        id: 6,
+        nome: "História de Amor e Música",
+        autor: "Autor não informado",
+        genero: "Drama",
+        preco: 39.90,
+        imagem: "drama/historiadeamoremusica.jpeg"
+    },
+
+    {
+        id: 7,
+        nome: "Pessoas Normais",
+        autor: "Sally Rooney",
+        genero: "Drama",
+        preco: 42.90,
+        imagem: "drama/pessoasnormais.jpeg"
+    },
+
+    {
+        id: 8,
+        nome: "Por Lugares Incríveis",
+        autor: "Jennifer Niven",
+        genero: "Drama",
+        preco: 39.90,
+        imagem: "drama/porlugaresincriveis.jpeg"
+    },
+
+    {
+        id: 9,
+        nome: "Quem é Você, Alasca?",
+        autor: "John Green",
+        genero: "Drama",
+        preco: 34.90,
+        imagem: "drama/quemveocealasa.jpeg"
+    },
+
+    {
+        id: 10,
+        nome: "Todas as Suas Imperfeições",
+        autor: "Colleen Hoover",
+        genero: "Drama",
+        preco: 42.90,
+        imagem: "drama/todasassuasimperfeicoes.jpeg"
+    },
+
+    {
+        id: 11,
+        nome: "Todo Esse Tempo",
+        autor: "Rachael Lippincott",
+        genero: "Drama",
+        preco: 39.90,
+        imagem: "drama/todoesse tempo.jpeg"
+    },
+
+
+    // =========================
+    // FANTASIA
+    // =========================
+
+    {
+        id: 12,
+        nome: "A Rainha do Nada",
+        autor: "Holly Black",
+        genero: "Fantasia",
+        preco: 44.90,
+        imagem: "fantasia/arainhadonada.jpeg"
+    },
+
+    {
+        id: 13,
+        nome: "Caraval",
+        autor: "Stephanie Garber",
+        genero: "Fantasia",
+        preco: 42.90,
+        imagem: "fantasia/caraval.jpeg"
+    },
+
+    {
+        id: 14,
+        nome: "Cidade dos Ossos",
+        autor: "Cassandra Clare",
+        genero: "Fantasia",
+        preco: 39.90,
+        imagem: "fantasia/cidadedosossos.jpeg"
+    },
+
+    {
+        id: 15,
+        nome: "Corte de Espinhos e Rosas",
+        autor: "Sarah J. Maas",
+        genero: "Fantasia",
+        preco: 44.90,
+        imagem: "fantasia/cortedeespinhos.jpeg"
+    },
+
+    {
+        id: 16,
+        nome: "Corte de Névoa e Fúria",
+        autor: "Sarah J. Maas",
+        genero: "Fantasia",
+        preco: 49.90,
+        imagem: "fantasia/corteden...jpeg"
+    },
+
+    {
+        id: 17,
+        nome: "Era Uma Vez um Coração Partido",
+        autor: "Stephanie Garber",
+        genero: "Fantasia",
+        preco: 44.90,
+        imagem: "fantasia/eraumavezumcoracaopartido.jpeg"
+    },
+
+    {
+        id: 18,
+        nome: "Harry Potter e a Pedra Filosofal",
+        autor: "J. K. Rowling",
+        genero: "Fantasia",
+        preco: 39.90,
+        imagem: "fantasia/harrypotterpedrafilosofal.jpeg"
+    },
+
+    {
+        id: 19,
+        nome: "O Hobbit",
+        autor: "J. R. R. Tolkien",
+        genero: "Fantasia",
+        preco: 39.90,
+        imagem: "fantasia/hobbit.jpeg"
+    },
+
+    {
+        id: 20,
+        nome: "As Crônicas de Nárnia",
+        autor: "C. S. Lewis",
+        genero: "Fantasia",
+        preco: 54.90,
+        imagem: "fantasia/narnia.jpeg"
+    },
+
+    {
+        id: 21,
+        nome: "O Rei Perverso",
+        autor: "Holly Black",
+        genero: "Fantasia",
+        preco: 44.90,
+        imagem: "fantasia/oreiperverso.jpeg"
+    },
+
+    {
+        id: 22,
+        nome: "Percy Jackson e os Olimpianos",
+        autor: "Rick Riordan",
+        genero: "Fantasia",
+        preco: 49.90,
+        imagem: "fantasia/percy...jpeg"
+    },
+
+    {
+        id: 23,
+        nome: "O Príncipe Cruel",
+        autor: "Holly Black",
+        genero: "Fantasia",
+        preco: 44.90,
+        imagem: "fantasia/principecruel.jpeg"
+    },
+
+    {
+        id: 24,
+        nome: "Quarta Asa",
+        autor: "Rebecca Yarros",
+        genero: "Fantasia",
+        preco: 59.90,
+        imagem: "fantasia/quartaasa.jpeg"
+    },
+
+    {
+        id: 25,
+        nome: "A Rainha Vermelha",
+        autor: "Victoria Aveyard",
+        genero: "Fantasia",
+        preco: 42.90,
+        imagem: "fantasia/rainhavermelha.jpeg"
+    },
+
+    {
+        id: 26,
+        nome: "Six of Crows",
+        autor: "Leigh Bardugo",
+        genero: "Fantasia",
+        preco: 49.90,
+        imagem: "fantasia/sixofcrows.jpeg"
+    },
+
+    {
+        id: 27,
+        nome: "Sombra e Ossos",
+        autor: "Leigh Bardugo",
+        genero: "Fantasia",
+        preco: 39.90,
+        imagem: "fantasia/sombraeossos.jpeg"
+    },
+
+    {
+        id: 28,
+        nome: "Trono de Vidro",
+        autor: "Sarah J. Maas",
+        genero: "Fantasia",
+        preco: 44.90,
+        imagem: "fantasia/tronodevidro.jpeg"
+    },
+
+
+    // =========================
+    // ROMANCE
+    // =========================
+
+    {
+        id: 29,
+        nome: "Amor Teoricamente",
+        autor: "Ali Hazelwood",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/amorteoricamente.jpeg"
+    },
+
+    {
+        id: 30,
+        nome: "Apostando no Amor",
+        autor: "Lynn Painter",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/apostandonoamor.jpeg"
+    },
+
+    {
+        id: 31,
+        nome: "Dates e Dragões",
+        autor: "Autor não informado",
+        genero: "Romance",
+        preco: 42.90,
+        imagem: "romance/datesedragons.jpeg"
+    },
+
+    {
+        id: 32,
+        nome: "De Férias com Você",
+        autor: "Emily Henry",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/deferiascomvoce.jpeg"
+    },
+
+    {
+        id: 33,
+        nome: "Dungeons & Drama",
+        autor: "Kristy Boyce",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/dungeonsedrama.jpeg"
+    },
+
+    {
+        id: 34,
+        nome: "É Assim que Acaba",
+        autor: "Colleen Hoover",
+        genero: "Romance",
+        preco: 44.90,
+        imagem: "romance/eassimqueacaba.jpeg"
+    },
+
+    {
+        id: 35,
+        nome: "É Assim que Começa",
+        autor: "Colleen Hoover",
+        genero: "Romance",
+        preco: 44.90,
+        imagem: "romance/eassimquecomeça.jpeg"
+    },
+
+    {
+        id: 36,
+        nome: "A Hipótese do Amor",
+        autor: "Ali Hazelwood",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/hipotesedoamor.jpeg"
+    },
+
+    {
+        id: 37,
+        nome: "Jogo de Amor para Dois",
+        autor: "Autor não informado",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/jogodeamorparadois.jpeg"
+    },
+
+    {
+        id: 38,
+        nome: "Lugar Feliz",
+        autor: "Emily Henry",
+        genero: "Romance",
+        preco: 42.90,
+        imagem: "romance/lugarfeliz.jpeg"
+    },
+
+    {
+        id: 39,
+        nome: "Melhor do que nos Filmes",
+        autor: "Lynn Painter",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/melhordoquenosfilmes.jpeg"
+    },
+
+    {
+        id: 40,
+        nome: "Não é como nos Filmes",
+        autor: "Lynn Painter",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/naoecomonosfilmes.jpeg"
+    },
+
+    {
+        id: 41,
+        nome: "Nem te Conto",
+        autor: "Autor não informado",
+        genero: "Romance",
+        preco: 34.90,
+        imagem: "romance/nemteconto.jpeg"
+    },
+
+    {
+        id: 42,
+        nome: "O Acordo",
+        autor: "Elle Kennedy",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/oacordo.jpeg"
+    },
+
+    {
+        id: 43,
+        nome: "Patinando no Amor",
+        autor: "Autor não informado",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/patinandonoamor.jpeg"
+    },
+
+    {
+        id: 44,
+        nome: "Sem Defeitos",
+        autor: "Autor não informado",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/semdefeitos.jpeg"
+    },
+
+    {
+        id: 45,
+        nome: "Sorte no Amor",
+        autor: "Autor não informado",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/sortenoamor.jpeg"
+    },
+
+    {
+        id: 46,
+        nome: "Teto para Dois",
+        autor: "Beth O'Leary",
+        genero: "Romance",
+        preco: 39.90,
+        imagem: "romance/tetoparadois.jpeg"
+    },
+
+
+    // =========================
+    // SUSPENSE
+    // =========================
+
+    {
+        id: 47,
+        nome: "A Empregada",
+        autor: "Freida McFadden",
+        genero: "Suspense",
+        preco: 42.90,
+        imagem: "suspense/A_Empregada.jpeg"
+    },
+
+    {
+        id: 48,
+        nome: "A Paciente Silenciosa",
+        autor: "Alex Michaelides",
+        genero: "Suspense",
+        preco: 42.90,
+        imagem: "suspense/A_Paciente_Silenciosa.jpeg"
+    },
+
+    {
+        id: 49,
+        nome: "A Última Festa",
+        autor: "Lucy Foley",
+        genero: "Suspense",
+        preco: 39.90,
+        imagem: "suspense/A_Ultima_Festa.jpeg"
+    },
+
+    {
+        id: 50,
+        nome: "Dias Perfeitos",
+        autor: "Raphael Montes",
+        genero: "Suspense",
+        preco: 39.90,
+        imagem: "suspense/Dias_Perfeitos.jpeg"
+    },
+
+    {
+        id: 51,
+        nome: "E Não Sobrou Nenhum",
+        autor: "Agatha Christie",
+        genero: "Suspense",
+        preco: 39.90,
+        imagem: "suspense/E_Não_Sobrou_Nenhum.jpeg"
+    },
+
+    {
+        id: 52,
+        nome: "Jantar Secreto",
+        autor: "Raphael Montes",
+        genero: "Suspense",
+        preco: 39.90,
+        imagem: "suspense/Jantar_Secreto.jpeg"
+    },
+
+    {
+        id: 53,
+        nome: "Mentirosos",
+        autor: "E. Lockhart",
+        genero: "Suspense",
+        preco: 39.90,
+        imagem: "suspense/Mentirosos.jpeg"
+    },
+
+    {
+        id: 54,
+        nome: "O Homem de Giz",
+        autor: "C. J. Tudor",
+        genero: "Suspense",
+        preco: 42.90,
+        imagem: "suspense/O_Homem_de_Giz.jpeg"
+    },
+
+    {
+        id: 55,
+        nome: "Uma Família Feliz",
+        autor: "Raphael Montes",
+        genero: "Suspense",
+        preco: 39.90,
+        imagem: "suspense/Uma_Familia_Feliz.jpeg"
+    },
+
+    {
+        id: 56,
+        nome: "Verity",
+        autor: "Colleen Hoover",
+        genero: "Suspense",
+        preco: 44.90,
+        imagem: "suspense/Verity.jpeg"
+    },
+
+
+    // =========================
+    // TERROR
+    // =========================
+
+    {
+        id: 57,
+        nome: "Bird Box",
+        autor: "Josh Malerman",
+        genero: "Terror",
+        preco: 42.90,
+        imagem: "terror/Bird_Box.jpeg"
+    },
+
+    {
+        id: 58,
+        nome: "Coraline",
+        autor: "Neil Gaiman",
+        genero: "Terror",
+        preco: 34.90,
+        imagem: "terror/Coraline.jpeg"
+    },
+
+    {
+        id: 59,
+        nome: "Doutor Sono",
+        autor: "Stephen King",
+        genero: "Terror",
+        preco: 49.90,
+        imagem: "terror/Doutor_Sono.jpeg"
+    },
+
+    {
+        id: 60,
+        nome: "It: A Coisa",
+        autor: "Stephen King",
+        genero: "Terror",
+        preco: 49.90,
+        imagem: "terror/It_A_Coisa.jpeg"
+    },
+
+    {
+        id: 61,
+        nome: "Louca Obsessão",
+        autor: "Stephen King",
+        genero: "Terror",
+        preco: 42.90,
+        imagem: "terror/Louca_Obsessão.jpeg"
+    },
+
+    {
+        id: 62,
+        nome: "O Colecionador",
+        autor: "John Fowles",
+        genero: "Terror",
+        preco: 39.90,
+        imagem: "terror/O_Colecionador.jpeg"
+    },
+
+    {
+        id: 63,
+        nome: "O Iluminado",
+        autor: "Stephen King",
+        genero: "Terror",
+        preco: 49.90,
+        imagem: "terror/O_Iluminado.jpeg"
+    },
+
+    {
+        id: 64,
+        nome: "O Vilarejo",
+        autor: "Raphael Montes",
+        genero: "Terror",
+        preco: 39.90,
+        imagem: "terror/O_Vilarejo.jpeg"
+    },
+
+    {
+        id: 65,
+        nome: "Saboroso Cadáver",
+        autor: "Agustina Bazterrica",
+        genero: "Terror",
+        preco: 42.90,
+        imagem: "terror/Saboroso_Cadaver.jpeg"
+    },
+
+
+    // =========================
+    // FICÇÃO
+    // =========================
+
+    {
+        id: 66,
+        nome: "A Biblioteca da Meia-Noite",
+        autor: "Matt Haig",
+        genero: "Ficção",
+        preco: 42.90,
+        imagem: "ficção/abiliotecadameianoite.jpeg"
+    },
+
+    {
+        id: 67,
+        nome: "A Menina que Roubava Livros",
+        autor: "Markus Zusak",
+        genero: "Ficção",
+        preco: 44.90,
+        imagem: "ficção/ameninaqueroubavalivros.jpeg"
+    },
+
+    {
+        id: 68,
+        nome: "A Vida Invisível de Addie LaRue",
+        autor: "V. E. Schwab",
+        genero: "Ficção",
+        preco: 49.90,
+        imagem: "ficção/avidainvisiveldeaddielarue.jpeg"
+    },
+
+    {
+        id: 69,
+        nome: "Flores para Algernon",
+        autor: "Daniel Keyes",
+        genero: "Ficção",
+        preco: 39.90,
+        imagem: "ficção/floresparaalgernon.jpeg"
+    },
+
+    {
+        id: 70,
+        nome: "O Diário de Anne Frank",
+        autor: "Anne Frank",
+        genero: "Ficção",
+        preco: 34.90,
+        imagem: "ficção/odiariodeannefrank.jpeg"
+    },
+
+    {
+        id: 71,
+        nome: "Os Olhos para Todos",
+        autor: "Autor não informado",
+        genero: "Ficção",
+        preco: 39.90,
+        imagem: "ficção/osoleparatodos.jpeg"
+    },
+
+    {
+        id: 72,
+        nome: "Os Sete Maridos de Evelyn Hugo",
+        autor: "Taylor Jenkins Reid",
+        genero: "Ficção",
+        preco: 44.90,
+        imagem: "ficção/ossetemaridoosdedevelynhugo.jpeg"
+    },
+
+    {
+        id: 73,
+        nome: "Torto Arado",
+        autor: "Itamar Vieira Junior",
+        genero: "Ficção",
+        preco: 44.90,
+        imagem: "ficção/tortoarado.jpeg"
+    },
+
+    {
+        id: 74,
+        nome: "Tudo é Rio",
+        autor: "Carla Madeira",
+        genero: "Ficção",
+        preco: 39.90,
+        imagem: "ficção/tudoerio_carlamadeira.jpeg"
+    }
+];
+
+
+function encontrarProduto(id) {
+    return produtos.find(produto => Number(produto.id) === Number(id)) || null;
+}
